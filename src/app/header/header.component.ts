9712e792e51afc9e134c0291d7ae4079b47fa9f2
Jhonatan Souza <jhondev97@gmail.com>
2025-04-05 00:59:47 -0300

    header da aplicação

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-      
+    menuOpen = false;
+
+    toggleMenu() {
+      this.menuOpen = !this.menuOpen;
+    }
 }

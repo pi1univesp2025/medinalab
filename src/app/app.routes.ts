edc1fae91212b8557cc78d175fa0563881f3182c
Jhonatan Souza <jhondev97@gmail.com>
2025-04-09 00:11:18 -0300

    definindo rota do componente team

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AdminComponent } from './admin/admin.component';
+import { TeamComponent } from './team/team.component';
 
 export const routes: Routes = [
     {path: "", component: HomeComponent },
-    {path: "admin", component: AdminComponent}
+    {path: "admin", component: AdminComponent},
+    {path: "team", component: TeamComponent}
 ];

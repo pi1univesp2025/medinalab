30303e68bd1813f8ee75739ade6669b4974ccce5
Jhonatan Souza <jhondev97@gmail.com>
2025-04-17 18:34:47 -0300

    definindo rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,17 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AdminComponent } from './admin/admin.component';
 import { TeamComponent } from './team/team.component';
+import { ResearchComponent } from './research/research.component';
+import { PublicationsComponent } from './publications/publications.component';
+import { AwardsComponent } from './awards/awards.component';
+import { JoinComponent } from './join/join.component';
 
 export const routes: Routes = [
     {path: "", component: HomeComponent },
     {path: "admin", component: AdminComponent},
-    {path: "team", component: TeamComponent}
+    {path: "team", component: TeamComponent},
+    {path: "research", component: ResearchComponent},
+    {path: "publications", component: PublicationsComponent},
+    {path: "awards", component: AwardsComponent},
+    {path: "join", component: JoinComponent}
 ];

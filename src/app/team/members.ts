df638a8779c6696c10e2ca410e52064f7510da7a
Jhonatan Souza <jhondev97@gmail.com>
2025-05-15 19:14:55 -0300

    arrumando alguns bugs

--- src/app/team/members.ts
+++ src/app/team/members.ts
@@ -2,17 +2,13 @@ export const members = [
     {
         photo: "assets/membros/Tiago.jpg",
         name: "Tiago Medina, PhD",
-        function: "Group Leader – Translational Immuno-Oncology Assistant Professor, Department of Molecular Medicine University of Southern Denmark – Odense, Denmark 📧 medina@health.sdu.dk",
-        lattes: "http://lattes.cnpq.br/1191348718336874",
-        description: "Dr. Medina is a biomedical scientist with expertise in immunology and epigenetics. He holds a PhD in Immunology from the University of São Paulo (USP) and completed postdoctoral training at the Princess Margaret Cancer Centre in Toronto, Canada. His research focuses on the mechanisms that regulate immune responses in the tumor microenvironment, with an emphasis on identifying biomarkers and developing strategies to enhance the efficacy of immunotherapy."
+        function: "Group Leader – Translational Immuno-Oncology Assistant Professor, Department of Molecular Medicine University of Southern Denmark – Odense, Denmark",
     },
 
     {
         photo: "assets/membros/Gabi.jpg",
         name: "Gabriela Sarti Kinker, PhD",
-        function: "Research Scientist – Translational Immuno-Oncology A.C. Camargo Cancer Center – São Paulo, Brazil 📧 Email: gabriela.kinker@gmail.com",
-        lattes: "http://lattes.cnpq.br/3598903476101060",
-        description: "Dr. Kinker holds a PhD in General Physiology from the University of São Paulo and completed postdoctoral training at the A.C. Camargo Cancer Center. She integrates experimental and computational approaches in tumor immunology, with a focus on identifying prognostic biomarkers and therapeutic targets. Her background combines expertise in tumor biology, immunology, and bioinformatics."
+        function: "Research Scientist – Translational Immuno-Oncology A.C. Camargo Cancer Center – São Paulo, Brazil",
     },
     
     {
